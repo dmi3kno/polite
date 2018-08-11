64d85a28dbbba57d605a6ce7dc594ce326301b7b
dmi3kno <dperepolkin@gmail.com>
2018-08-11 08:50:00 +0200

    added option for specifying filename in rip()

--- R/rip.R
+++ R/rip.R
@@ -1,6 +1,7 @@
 #' Polite file download
 #'
 #' @param bow host introduction object of class polite, session created by bow() or nod
+#' @param new_filename optional new file name to use when saving the file
 #' @param suffix optional characters added to file name
 #' @param sep separator between file name and suffix. Default "__"
 #' @param path path where file should be saved. Defults to folder named "downloads" created in the working directory
@@ -19,16 +20,16 @@
 #' }
 #' @importFrom here here
 #' @importFrom tools file_path_sans_ext file_ext
-rip <- function(bow, suffix=NULL, sep="__", path="downloads", overwrite=FALSE, mode="wb", ...){
+rip <- function(bow, new_filename=NULL, suffix=NULL, sep="__", path="downloads", overwrite=FALSE, mode="wb", ...){
   url <- bow$url
   if(!dir.exists(here::here(path)))
     dir.create(here::here(path))
 
-  new_file_name <- paste0(tools::file_path_sans_ext(basename(url)), sep, suffix, ".", tools::file_ext(basename(url)), sep="")
+  new_file_name <- new_file_name %||% paste0(tools::file_path_sans_ext(basename(url)), sep, suffix, ".", tools::file_ext(basename(url)), sep="")
 
-  if(file.exists(here::here(path, basename(url))) && !overwrite){
+  if(file.exists(here::here(path, new_file_name)) && !overwrite){
     warning("File already exists", call. = FALSE)
-    return(here::here(path, basename(url)))
+    return(here::here(path, new_file_name))
     }
 
   bow$download_file_ltd(url, here::here(path, new_file_name), mode=mode, ...)
